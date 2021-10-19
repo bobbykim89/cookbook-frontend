ec709a84adc88278637f1e9b6c3f61993f89298a
bobbykim89 <bobby.sihun.kim@gmail.com>
2021-10-19 01:17:56 -0700

    added query to get extended logged in user info

--- queries/userQueries.js
+++ queries/userQueries.js
@@ -36,4 +36,44 @@ const ME = gql`
   }
 `;
 
-export { GET_USER, ME };
+const GET_USER_INFO = gql`
+  query UserInfo {
+    me {
+      email
+      username
+      id
+      user {
+        profile {
+          introduction
+          id
+          avatar {
+            id
+            url
+            formats
+          }
+        }
+        recipes {
+          title
+          ingredients
+          direction
+          user {
+            username
+            id
+          }
+          category {
+            name
+            id
+          }
+          cover {
+            id
+            url
+            formats
+          }
+          created_at
+        }
+      }
+    }
+  }
+`;
+
+export { GET_USER, ME, GET_USER_INFO };
